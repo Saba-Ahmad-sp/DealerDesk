f96ec29e5fd0a4e47c3d7bbd5ccec7603a20d2d0
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-06-05 23:59:23 +0530

    Adds Home page and Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const isFormValid = email.trim() !== '' && password.trim() !== '';
+
+  const isEmailValid = (email: string) =>
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  const handleLogin = () => {
+    if (!isEmailValid(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+
+    const storedUser = localStorage.getItem('popxUser');
+    if (!storedUser) {
+      alert('No user found. Please create an account.');
+      return;
+    }
+
+    const parsedUser = JSON.parse(storedUser);
+    if (parsedUser.email === email && parsedUser.password === password) {
+      localStorage.setItem('isLoggedIn', 'true');
+      router.push('/account');
+    } else {
+      alert('Invalid credentials. Please try again.');
+    }
+  };
+
+  return (
+    <div className="bg-[#0f1924] min-h-screen flex items-center justify-center text-white px-5 sm:p-8">
+      <div className="w-full max-w-lg sm:p-8">
+        {/* Header */}
+        <div className="mb-[28px] text-center">
+          <h2 className="text-3xl sm:text-4xl font-bold mb-2.5">
+            Sign in to DealerDesk
+          </h2>
+          <p className="text-gray-300 px-6 text-[18px]">
+            Access your account and manage orders
+          </p>
+        </div>
+
+        {/* Inputs */}
+        <div className="flex flex-col gap-6">
+          <div className="relative">
+            <span className="absolute -top-2 left-3 bg-[#0f1924] px-1 text-xs text-[#5d90b9] font-medium">
+              Email Address
+            </span>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email address"
+              className="w-full border border-gray-600 bg-transparent text-white rounded-md py-2 px-3 pt-4 text-sm outline-none focus:ring-1 focus:ring-[#5d90b9]"
+            />
+          </div>
+
+          <div className="relative">
+            <span className="absolute -top-2 left-3 bg-[#0f1924] px-1 text-xs text-[#5d90b9] font-medium">
+              Password
+            </span>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+              className="w-full border border-gray-600 bg-transparent text-white rounded-md py-2 px-3 pt-4 text-sm outline-none focus:ring-1 focus:ring-[#5d90b9]"
+            />
+          </div>
+        </div>
+
+        {/* Button */}
+        <button
+          onClick={handleLogin}
+          disabled={!isFormValid}
+          className={`mt-6 py-3 w-full rounded font-semibold transition ${
+            isFormValid
+              ? 'bg-[#315572] hover:bg-[#587995] text-white cursor-pointer'
+              : 'bg-gray-700 text-gray-400 cursor-not-allowed'
+          }`}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  return redirect('/dashboard');
+   return (
+    
+      <div className="bg-[#0f1924] w-full h-screen sm:p-8 px-5 flex flex-col justify-center items-center text-white">
+        <div className="w-full max-w-lg">
+        {/* Header */}
+        <div className="mb-[28px] text-center">
+          <h1 className="text-3xl sm:text-4xl font-bold mb-2.5 ">
+            Welcome to DealerDesk
+          </h1>
+          <p className="text-gray-300 px-6 text-[18px]">
+            Mini Dealer Portal ! B2B ordering platform
+          </p>
+        </div>
+
+        {/* Buttons */}
+        <div className="flex flex-col gap-2.5">
+          <Link
+            href="/register"
+            className="bg-[#315572] text-white py-[13px] text-center rounded font-bold hover:bg-[#587995] transition"
+          >
+            Create Account
+          </Link>
+          <Link
+            href="/login"
+            className="bg-[#253748] py-[13px] text-center rounded font-bold hover:bg-[#3e5367] transition"
+          >
+            Already Registered? Login
+          </Link>
+        </div>
+        </div>
+      </div>
+  );
 }
